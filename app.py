88c704e9ee87726d3d6fe930f292e0354c3e3cf4
bashhack <info@marclaughton.com>
2016-01-24 20:31:27 -0600

    adding in 404 handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import (Flask, g, render_template, flash, redirect, url_for)
+from flask import (Flask, g, render_template, flash, redirect, url_for, abort)
 from flask.ext.bcrypt import check_password_hash
 from flask.ext.login import (LoginManager, login_user, logout_user,
                              login_required, current_user)
@@ -96,8 +96,14 @@ def post():
 def stream(username=None):
     template = 'stream.html'
     if username and username != current_user.username:
-        user = models.User.select().where(models.User.username**username).get()
-        stream = user.posts.limit(100)
+        try:
+            user = models.User.select().where(
+                models.User.username**username
+            ).get()
+        except models.DoesNotExist:
+            abort(404)
+        else:
+            stream = user.posts.limit(100)
     else:
         stream = current_user.get_stream().limit(100)
         user = current_user
@@ -109,6 +115,8 @@ def stream(username=None):
 @app.route('/post/<int:post_id>')
 def view_post(post_id):
     posts = models.Post.select().where(models.Post.id == post_id)
+    if posts.count() == 0:
+        abort(404)
     return render_template('stream.html', stream=posts)
 
 
@@ -118,7 +126,7 @@ def follow(username):
     try:
         to_user = models.User.get(models.User.username**username)
     except models.DoesNotExist:
-        pass
+        abort(404)
     else:
         try:
             models.Relationship.create(
@@ -140,7 +148,7 @@ def unfollow(username):
     try:
         to_user = models.User.get(models.User.username**username)
     except models.DoesNotExist:
-        pass
+        abort(404)
     else:
         try:
             models.Relationship.get(
@@ -160,6 +168,11 @@ def unfollow(username):
 def index():
     stream = models.Post.select().limit(100)
     return render_template('stream.html', stream=stream)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     models.initialize()
